f8a19aafdbb3aedc3c7bd5ca1d8c54e564de987f
Shae Kuronen <shae.kuronen@gmail.com>
2014-03-27 06:09:40 -0700

    added demo 2 slideshow

--- js/site/flexslider_init.js
+++ js/site/flexslider_init.js
@@ -20,6 +20,24 @@
     });
     // end demo 1 slideshow
 
+    // demo 2 slideshow
+    $('#flexloader-demo-2-slideshow').flexslider({
+      namespace: "flexloader-demo-2-",
+      animation: "slide",
+      prevText: "prev",
+      nextText: "next",
+      keyboard: false,
+      slideshowSpeed: 100000,
+      animationSpeed: 300,
+      start: function(slider) {
+        GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
+      },
+      after: function(slider) {
+        GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
+      }
+    });
+    // end demo 2 slideshow
+
   });
 
 }(jQuery));
