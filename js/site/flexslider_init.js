4f0a749785ad955e2f801b48399993fbe9110039
Shae Kuronen <shae.kuronen@gmail.com>
2014-03-29 20:47:44 -0700

    added a flexslider only demo

--- js/site/flexslider_init.js
+++ js/site/flexslider_init.js
@@ -6,17 +6,9 @@
     $('#flexloader-demo-1-slideshow').flexslider({
       namespace: "flexloader-demo-1-",
       animation: "slide",
-      prevText: "prev",
-      nextText: "next",
       keyboard: false,
       slideshowSpeed: 100000,
-      animationSpeed: 300,
-      start: function(slider) {
-        GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
-      },
-      after: function(slider) {
-        GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
-      }
+      animationSpeed: 300
     });
     // end demo 1 slideshow
 
@@ -30,15 +22,33 @@
       slideshowSpeed: 100000,
       animationSpeed: 300,
       start: function(slider) {
-        console.log('demo 2 start event');
         GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
       },
       after: function(slider) {
-        console.log('demo 2 after animate event');
         GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
       }
     });
     // end demo 2 slideshow
+
+    // demo 3 slideshow
+    $('#flexloader-demo-3-slideshow').flexslider({
+      namespace: "flexloader-demo-3-",
+      animation: "slide",
+      prevText: "prev",
+      nextText: "next",
+      keyboard: false,
+      slideshowSpeed: 100000,
+      animationSpeed: 300,
+      start: function(slider) {
+        console.log('demo 3 start event');
+        GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
+      },
+      after: function(slider) {
+        console.log('demo 3 after animate event');
+        GLOBAL.flexslider_lazyloader.load_adjacent_slides(slider);
+      }
+    });
+    // end demo 3 slideshow
 
   });
 
